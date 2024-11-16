5cd87254f276be5a1625191cc31af54e2811f88e
KSAhh <66674138+KSAhh@users.noreply.github.com>
2024-11-16 20:10:05 +0900

    백엔드 카카오 로그인

--- back/algofipo_pjt/settings.py
+++ back/algofipo_pjt/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-x=0joi*19xdl7gh8j!q8&9@*yb(g#b)p=hz9kg!eafpjh2cs$k'
+SECRET_KEY = "django-insecure-x=0joi*19xdl7gh8j!q8&9@*yb(g#b)p=hz9kg!eafpjh2cs$k"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -31,63 +31,127 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'accounts',
-    'articles',
-    'rest_framework',   # serializer
-    'corsheaders',      # CORS header
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
+    # my app
+    "accounts",
+    "articles",
+    
+    # third party
+    "rest_framework",  # serializer
+    "rest_framework.authtoken",  # Token authentication
+    "dj_rest_auth",  # authentication library
+    "corsheaders",  # CORS header
+
+    # authentication registration
+    "django.contrib.sites",
+    "allauth",
+    "allauth.account",
+    "allauth.socialaccount",
+    "allauth.socialaccount.providers.google",
+    "allauth.socialaccount.providers.kakao",
+    "allauth.socialaccount.providers.naver",
+    "dj_rest_auth.registration",
+
+    "django.contrib.admin",
+    "django.contrib.auth",
+    "django.contrib.contenttypes",
+    "django.contrib.sessions",
+    "django.contrib.messages",
+    "django.contrib.staticfiles",
 ]
 
+SITE_ID = 1  # authentication registration
+
+# SOCIALACCOUNT_LOGIN_ON_GET = True
+# LOGIN_REDIRECT_URL = '/'
+# ACCOUNT_LOGOUT_ON_GET = True 
+# LOGIN_REDIRECT_URL = '/'  # 로그인 후 리다이렉트 될 경로
+# ACCOUNT_LOGOUT_REDIRECT_URL = reverse_lazy('accountapp:login')
+# AllAuth settings
+ACCOUNT_USER_MODEL_USERNAME_FIELD = None
+ACCOUNT_EMAIL_REQUIRED = True
+ACCOUNT_USERNAME_REQUIRED = False
+ACCOUNT_AUTHENTICATION_METHOD = 'email'
+ACCOUNT_EMAIL_VERIFICATION = 'none'
+# dj-rest-auth
+REST_USE_JWT = True
+JWT_AUTH_COOKIE = 'my-app-auth'
+
+SOCIALACCOUNT_PROVIDERS = {
+    'kakao': {
+        'APP': {
+            'client_id': '20eda2ad564bc5141caa67bee72893f4',  # 카카오 REST API 키
+            'secret': 'qPaDqzJDpklw9pUyHSXBiTVQB5HhLaNA', 
+            "key": "",
+        }
+    }
+}
+
+REST_FRAMEWORK = {
+    # Authentication - Token 인증 방식
+    "DEFAULT_AUTHENTICATION_CLASSES": [
+        "rest_framework.authentication.TokenAuthentication",
+    ],
+    # permission
+    "DEFAULT_PERMISSION_CLASSES": [
+        "rest_framework.permissions.AllowAny",
+    ],
+}
+
+# Django Allauth
+AUTHENTICATION_BACKENDS = [
+    'django.contrib.auth.backends.ModelBackend', # 기본 인증
+    'allauth.account.auth_backends.AuthenticationBackend', # allauth 인증
+]
+# Custom SocialAccount Adapter 설정
+SOCIALACCOUNT_ADAPTER = 'accounts.adapter.CustomSocialAccountAdapter'
+
 MIDDLEWARE = [
-    'django.middleware.security.SecurityMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'corsheaders.middleware.CorsMiddleware', # CORS header
-    'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    "django.middleware.security.SecurityMiddleware",
+    "django.contrib.sessions.middleware.SessionMiddleware",
+    "corsheaders.middleware.CorsMiddleware",  # CORS header
+    "django.middleware.common.CommonMiddleware",
+    "django.middleware.csrf.CsrfViewMiddleware",
+    "django.contrib.auth.middleware.AuthenticationMiddleware",
+    "django.contrib.messages.middleware.MessageMiddleware",
+    "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    # Add the account middleware:
+    "allauth.account.middleware.AccountMiddleware",  # authentication registration
 ]
 
 # CORS header - Add Domain
 CORS_ALLOWED_ORIGINS = [
-    'http://127.0.0.1:5173',
-    'http://localhost:5173',
+    "http://127.0.0.1:5173",
+    "http://localhost:5173",
 ]
 
-ROOT_URLCONF = 'algofipo_pjt.urls'
+ROOT_URLCONF = "algofipo_pjt.urls"
 
 TEMPLATES = [
     {
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [ BASE_DIR / 'templates' ],
-        'APP_DIRS': True,
-        'OPTIONS': {
-            'context_processors': [
-                'django.template.context_processors.debug',
-                'django.template.context_processors.request',
-                'django.contrib.auth.context_processors.auth',
-                'django.contrib.messages.context_processors.messages',
+        "BACKEND": "django.template.backends.django.DjangoTemplates",
+        "DIRS": [BASE_DIR / "templates"],
+        "APP_DIRS": True,
+        "OPTIONS": {
+            "context_processors": [
+                "django.template.context_processors.debug",
+                "django.template.context_processors.request",
+                "django.contrib.auth.context_processors.auth",
+                "django.contrib.messages.context_processors.messages",
             ],
         },
     },
 ]
 
-WSGI_APPLICATION = 'algofipo_pjt.wsgi.application'
+WSGI_APPLICATION = "algofipo_pjt.wsgi.application"
 
 
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
     }
 }
 
@@ -97,16 +161,16 @@ DATABASES = {
 
 AUTH_PASSWORD_VALIDATORS = [
     {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+        "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+        "NAME": "django.contrib.auth.password_validation.MinimumLengthValidator",
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+        "NAME": "django.contrib.auth.password_validation.CommonPasswordValidator",
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+        "NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",
     },
 ]
 
@@ -114,9 +178,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = "en-us"
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = "UTC"
 
 USE_I18N = True
 
@@ -126,18 +190,18 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = "static/"
 
 # Media files (Images)
 import os
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+MEDIA_URL = "/media/"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 # Custom User
-AUTH_USER_MODEL = 'accounts.User'
+AUTH_USER_MODEL = "accounts.User"
--- back/algofipo_pjt/urls.py
+++ back/algofipo_pjt/urls.py
@@ -16,7 +16,6 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from accounts import views as accounts_views
 
 # Media files
 from django.conf import settings
@@ -25,11 +24,26 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
 
-    path('api-auth/', include('rest_framework.urls')), # DRF 기본 인증뷰
-
-    path('accounts/', include('accounts.urls')),
-    path('<int:user_pk>/password/', accounts_views.update_password, name='update_password'), # 유저 비밀번호 변경
+    # Articles API
     path('api/v1/', include('articles.urls')),
+
+    # Authentication
+    path("accounts/", include("dj_rest_auth.urls")),  # 로그인, 로그아웃, 비밀번호 변경 등
+    path("accounts/signup/", include("dj_rest_auth.registration.urls")),  # 회원가입 관련 URL
+    path('accounts/social/', include('accounts.urls')),
+    path("accounts/allauth/", include("allauth.urls")),  
+
+    # path('accounts/', include('accounts.urls')),
+    # path('accounts/', include('allauth.urls')),
+    # path("accounts/social/", include("allauth.urls")),  # 소셜 로그인 URL (allauth)
+    # path("accounts/kakao/", include("allauth.socialaccount.urls")),  # 소셜 로그인
+    # path("api/social/", include("allauth.socialaccount.urls")),  # 소셜 로그인
+
+    # path('api-auth/', include('rest_framework.urls')), # DRF 기본 인증뷰 (세션인증)
+    # path('<int:user_pk>/password/', accounts_views.update_password, name='update_password'), # 유저 비밀번호 변경
+
 ]
+
+
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
