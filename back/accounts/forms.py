17de7df61eb7cf743fa49289bd82e391518d5dc0
KSAhh <66674138+KSAhh@users.noreply.github.com>
2024-11-18 01:52:39 +0900

    Feat: 유저 회원가입 & 소셜로그인
    카카오로그인 구현
    DRF기반 유저 회원가입/회원탈퇴/로그인/로그아웃 구현
    
    Related to #3

--- back/accounts/forms.py
+++ back/accounts/forms.py
@@ -1,13 +1,14 @@
 from django.contrib.auth import get_user_model
 from django.contrib.auth.forms import UserCreationForm, UserChangeForm
 
+User = get_user_model()
 # 유저 회원가입
 class CustomUserCreationForm(UserCreationForm):
     class Meta(UserCreationForm.Meta):
-        model = get_user_model()
+        model = User
 
 # 유저 정보수정
 class CustomUserChangeForm(UserChangeForm):
     class Meta(UserChangeForm.Meta):
-        model = get_user_model()
+        model = User
         fields = ('first_name', 'last_name', 'email',)
--- front/temp-kakao-login/vue-project/src/main.js
+++ front/temp-kakao-login/vue-project/src/main.js
@@ -6,11 +6,10 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 
+const KAKAO_LOGIN_JS_KEY = import.meta.env.VITE_KAKAOLOGIN_JS_KEY
+Kakao.init(KAKAO_LOGIN_JS_KEY);
 const app = createApp(App)
-
-
 
 app.use(createPinia())
 app.use(router)
-
 app.mount('#app')
