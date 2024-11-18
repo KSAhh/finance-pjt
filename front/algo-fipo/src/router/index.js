2cb3c18ac3b4f8614bdbfae805cf46cb95b39c17
백지원 <olqkfh@naver.com>
2024-11-18 22:22:24 +0900

    feat: NavBar dropdown 구현, LoginPage 구현

--- front/algo-fipo/src/router/index.js
+++ front/algo-fipo/src/router/index.js
@@ -1,13 +1,19 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import MainView from '@/views/MainView.vue'
+import LoginView from "@/views/LoginView.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
-      name: 'Main',
+      name: 'MainView',
       component: MainView,
+    },
+    {
+      path: "/login",
+      name: "LoginView",
+      component: LoginView, // LoginView 컴포넌트 등록
     },
   ],
 })
