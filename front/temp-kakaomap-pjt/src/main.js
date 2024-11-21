3ed390670775bec945de55180a114b91081e18ee
KSAhh <dk-more@daum.net>
2024-11-21 11:22:41 +0900

    Feat: 지도검색 기능 일부구현

--- front/temp-kakaomap-pjt/src/main.js
+++ front/temp-kakaomap-pjt/src/main.js
@@ -1,11 +1,14 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import { useKakao } from 'vue3-kakao-maps/@utils';
 
 import App from './App.vue'
 import router from './router'
 
-const app = createApp(App)
+// 카카오API 사용 및 라이브러리 추가
+useKakao(import.meta.env.VITE_KAKAO_JS_KEY, ['clusterer', 'services', 'drawing']); 
 
+const app = createApp(App)
 app.use(createPinia())
 app.use(router)
 
