9dbc7afb2e65bec3dfc228e95d1ffcb4b89b50fc
KSAhh <dk-more@daum.net>
2024-11-21 18:04:16 +0900

    Refactor : products DB 변경
    유저가입 상품 저장 위한 테이블 추가
    생성 API 기능 추가

--- back/products/models.py
+++ back/products/models.py
@@ -1,6 +1,7 @@
 from django.db import models
-from django.contrib.contenttypes.fields import GenericForeignKey
+from django.contrib.contenttypes.fields import GenericForeignKey, GenericRelation
 from django.contrib.contenttypes.models import ContentType
+
 from django.contrib.auth import get_user_model
 User = get_user_model()
 
@@ -21,13 +22,13 @@ class AbstractProduct(models.Model):
     class Meta:
         abstract = True  # 부모 클래스는 테이블 생성 안 함
     
-# 금융상품 - 예금
+# 금융상품 - 예금 전용필드
 class DepositProduct(AbstractProduct):
-    pass # 전용필드
+    options = GenericRelation('ProductOption')  # GenericRelation 추가 (역참조 위함)
     
-# 금융상품 - 적금
+# 금융상품 - 적금 전용필드
 class SavingProduct(AbstractProduct):
-    pass # 전용필드
+    options = GenericRelation('ProductOption')  # GenericRelation 추가 (역참조 위함)
 
 
 # 금융상품 옵션
@@ -48,15 +49,26 @@ class ProductOption(models.Model):
 
 
 # 유저가 가입한 금융상품
-# class UserProduct(models.Model):
-#     user = models.ForeignKey(User, on_delete=models.CASCADE)  # 사용자를 참조
-#     product = models.ForeignKey(Product, on_delete=models.CASCADE)  # 상품을 참조
-#     product_type = models.CharField(max_length=50)  # 상품 유형 (예: 예금, 적금)
-#     balance = models.DecimalField(max_digits=20, decimal_places=2, default=0)  # 상품에 남은 잔액
-#     start_date = models.DateField()  # 상품 가입 날짜
-#     end_date = models.DateField(null=True, blank=True)  # 상품 만기 날짜
-#     status = models.CharField(max_length=20, default="active")  # 상태 (예: active, completed)
-#     created_at = models.DateTimeField(auto_now_add=True)  # 레코드 생성 시간
+class UserProduct(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)  # 유저 참조
+    product_type = models.CharField(max_length=50, choices=(("deposit", "Deposit"), ("saving", "Saving"), ("etc", "Etc")), default="etc")  # 상품 유형
+    
+    # 예금/적금/기타 상품 중 택1
+    deposit_product = models.ForeignKey(DepositProduct, null=True, blank=True, on_delete=models.SET_NULL, related_name="user_products")  # 예금 상품
+    saving_product = models.ForeignKey(SavingProduct, null=True, blank=True, on_delete=models.SET_NULL, related_name="user_products")  # 적금 상품
+    etc_product = models.CharField(null=True, blank=True, max_length=50, default="Unknown")
 
-#     def __str__(self):
-#         return f"{self.user.username} - {self.product.fin_prdt_nm}"
+    balance = models.DecimalField(max_digits=20, decimal_places=2, default=0.00)  # 상품에 남은 잔액
+    start_date = models.DateField(null=True, blank=True)    # 상품 가입일
+    end_date = models.DateField(null=True, blank=True)      # 상품 만기일
+    status = models.CharField(
+            max_length=20,
+            choices=(("active", "Active"), ("completed", "Completed"), ("canceled", "Canceled")),
+            default="active",
+        )  # 상태 (active, completed, canceled)
+    
+
+    def __str__(self):
+        product_name = (
+            self.deposit_product.fin_prdt_nm if self.product_type == "deposit" else self.saving_product.fin_prdt_nm)
+        return f"{self.user.username} - {product_name}"
