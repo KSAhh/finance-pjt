7d3db717eda669f31bc0dc4d950ae16b7f108db7
KSAhh <66674138+KSAhh@users.noreply.github.com>
2024-11-25 03:32:55 +0900

    Feat: 고객센터 글 조회기능 구현
    Related to #24

--- front/algo-fipo/src/views/CustomerSupportView.vue
+++ front/algo-fipo/src/views/CustomerSupportView.vue
@@ -1,13 +1,6 @@
 <template>
     <h1>고객센터</h1>
-    <div v-if="store.articles.length === 0">작성된 글이 없습니다.</div>
-    <div v-else>
-        <CSList 
-            v-for="article in store.articles"
-            :key="article.id"
-            :article="article"
-        />
-    </div>
+    <CSList />
 </template>
 
 <script setup>
